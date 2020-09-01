d4c8be4fc860e7871f1f4171dd5067a61c0104d5
rrortegaa <rrortegaa@gmail.com>
2020-09-01 02:46:13 -0500

    add Introduction component

---
+++ components/Introduction.jsx
@@ -0,0 +1,25 @@
+
+export default function Introduction() {
+
+  return (
+    <div className="introduction">
+      <div className="container">
+        <div className="card">
+          <div className="container">
+            <div className='info-section'>
+              <div className='title'>
+              </div>
+              <div className='text'>
+              </div>
+              <div className='subtitle'>
+              </div>
+            </div>
+            <div className='image-section'>
+              <img className='image' src="" alt=""/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
