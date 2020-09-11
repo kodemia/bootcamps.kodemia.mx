b04b9d33317e5b3e5ea0dc7d7616640fabde6ab5
rrortegaa <rrortegaa@gmail.com>
2020-09-10 20:02:06 -0500

    add KodeAwards component

--- components/Scholarship.jsx
+++ components/Scholarship.jsx
@@ -2,14 +2,14 @@ import Link from 'next/link'
 
 export default function Scholarship () {
   return (
-    <div className='scholarship'>
+    <section className='scholarship'>
       <div className='container'>
         <div className='card'>
-          <div className='title'>aplicar por una beca del 5% hasta el 15%</div>
+          <div className='title'>Aplicar por una beca del 5% hasta el 15%</div>
           <div className='text'>
-            otorgamos becas basadas en tu actitud y tu talento.
-             Puedes aplicar a través de una entrevista personalizada,
-             durante la entrevista te diremos si es la resolución de tu solicitud
+            Otorgamos becas basadas en tu actitud y tu talento.{' '}
+            Puedes aplicar a través de una entrevista personalizada, {' '}
+            durante la entrevista te diremos si es la resolución de tu solicitud
           </div>
           <div className='button'>
             <Link href='/interview'>
@@ -18,6 +18,6 @@ export default function Scholarship () {
           </div>
         </div>
       </div>
-    </div>
+    </section>
   )
 }
