e0accfbbad68e80b462d70d33e1bfec6d5bc1de6
rrortegaa <rrortegaa@gmail.com>
2020-09-22 04:37:03 -0500

    add useState hooks for set submit errors and when form is submitting, add new components for fetch and phone input

--- components/FormInterview.jsx
+++ components/FormInterview.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
-import Link from 'next/link'
-import { useForm } from 'react-hook-form'
+import React, { useState } from 'react'
+import fetch from 'node-fetch'
+import { useRouter } from 'next/router'
 
-export default function FormInterview () {
-  const { register, handleSubmit, errors } = useForm()
-  const onSubmit = data => console.log(data)
-  console.log(errors)
+import { useForm, Controller } from 'react-hook-form'
+import PhoneInput from 'react-phone-input-2'
+import es from 'react-phone-input-2/lang/es.json'
+
+export default function FormInterview ({ language, modality }) {
+  const router = useRouter()
+  const { register, handleSubmit, errors, control } = useForm()
+  const [submitError, setSubmitError] = useState('')
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const onSubmit = async data => {
+    setIsSubmitting(true)
+    try {
+      const resp = await fetch('https://httpstat.us/400', {
+        method: 'POST',
+        body: JSON.stringify({ ...data, language, modality }),
+        headers: { 'Content-Type': 'application/json' }
+      })
+      if (!resp.ok) throw new Error('¡Algo salió mal! intentalo más tarde')
+      setIsSubmitting(false)
+      router.push('entrevista/gracias')
+    } catch (error) {
+      setIsSubmitting(false)
+      setSubmitError(error.message)
+    }
+  }
 
   return (
     <div className='register-form brochure-form'>
@@ -16,16 +38,20 @@ export default function FormInterview () {
           name='firstName'
           placeholder='Nombre'
           maxLength={80}
-          ref={register({ required: true })}
+          ref={register({ required: 'Tu nombre es requerido' })}
+          required
           type='text'
         />
+        {errors.firstName &&
+          <p> {errors.firstName.message} </p>}
         <label className='input-label' htmlFor='lastName'>Apellido</label>
         <input
           className='input-field'
           name='lastName'
           placeholder='Apellido'
           maxLength={100}
-          ref={register({ required: true })}
+          ref={register({ required: 'Tu apellido es requerido' })}
+          required
           type='text'
         />
         <label className='input-label' htmlFor='email'>Email</label>
@@ -35,28 +61,49 @@ export default function FormInterview () {
           placeholder='correo@example.com'
           ref={
             register({
-              required: true,
-              pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              required: 'Tu email es requerido',
+              pattern: {
+                message: 'Ingresa un email válido',
+                value: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              }
             })
           }
+          required
           type='email'
         />
         <label className='input-label' htmlFor='phone'>Teléfono con WhatsApp</label>
-        <input
-          className='input-field'
+        <Controller
           name='phone'
-          placeholder='55 3333 5555'
-          ref={
-            register({
-              required: true,
-              minLength: 10,
-              maxLength: 13
-            })
-          }
-          type='tel'
+          control={control}
+          as={(
+            <PhoneInput
+              className='input-field'
+              country='mx'
+              regions={['america']}
+              preferredCountries={['mx', 'co', 'pe', 've', 'ar']}
+              localization={es}
+              countryCodeEditable={false}
+              placeholder='55 3333 5555'
+              type='tel'
+              defaultMask='..-....-....'
+              alwaysDefaultMask
+              inputProps={{
+                name: 'phone',
+                required: true,
+                autoFocus: false,
+                pattern: '^[\\+]?[(]?[0-9]{2,}[)]?[-\\s\\.]?[0-9]{2,}[-\\s\\.]?[0-9]{2,}[-\\s\\.]?[0-9]{2,}$'
+              }}
+            />
+          )}
         />
+
         <div className='pretty  p-bigger p-default p-curve p-smooth checkbox'>
-          <input type='checkbox' name='termsAndConditions' ref={register({ required: true })} />
+          <input
+            type='checkbox'
+            name='termsAndConditions'
+            ref={register({ required: 'Es necesario que aceptes los términos y condiciones' })}
+            required
+          />
           <div className='state p-info-o'>
             <label>
               Acepto los
@@ -66,9 +113,18 @@ export default function FormInterview () {
             </label>
           </div>
         </div>
-        <Link href='/javascript-lifetime/entrevista/gracias'>
-          <input className='submit' type='submit' />
-        </Link>
+        <input
+          className='submit'
+          type='submit'
+          value={isSubmitting ? 'Enviando...' : 'Enviar'}
+          disabled={isSubmitting}
+        />
+        {
+          submitError &&
+            <div className='error-message'>
+              {submitError}
+            </div>
+        }
       </form>
     </div>
   )
